901eeaed18cadfdeaf8e13b8c8585972abed5a30
Reza Qadimi <rezaqadimi.ir@gmail.com>
2021-06-02 21:51:10 +0430

    Add project files.

---
+++ MyApplication/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace MyApplication
+{
+	static class Program
+	{
+		//******************************************
+		#region Static Constructure
+		//******************************************
+		/// <summary>
+		/// Static Constructure
+		/// </summary>
+		static Program()
+		{
+		}
+		//******************************************
+		#endregion /Static Constructure
+		//******************************************
+
+		//******************************************
+		#region Main [ Method ]
+		//******************************************
+		[System.STAThread]
+		internal static void Main()
+		{
+			//***************************************
+			System.Windows.Forms.Application.EnableVisualStyles();
+
+			System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false);
+			//***************************************
+
+			//***************************************
+			Infrastructure.BaseForm startupForm = new StartupForm();
+
+			System.Windows.Forms.Application.Run(mainForm: startupForm);
+			//***************************************
+
+			//***************************************
+			if (startupForm != null)
+			{
+				if (startupForm.IsDisposed is false)
+				{
+					startupForm.Dispose();
+					//startupForm = null;
+				}
+			}
+			//***************************************
+		}
+		//******************************************
+		#endregion Main [ Method ]
+		//******************************************
+	}
+}
---
+++ MyApplication/StartupForm.cs
@@ -0,0 +1,15 @@
+﻿namespace MyApplication
+{
+	public partial class StartupForm : Infrastructure.BaseForm
+	{
+		#region Default Constructor:
+		/// <summary>
+		/// Default Constructor
+		/// </summary>
+		public StartupForm() : base()
+		{
+			InitializeComponent();
+		}
+		#endregion /Default Constructor
+	}
+}
