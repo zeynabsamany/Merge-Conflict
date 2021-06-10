1fbb0af0c2170082482578dea9e65d3001b57026
zeinab <zeynabsamany@gmail.com>
2021-06-10 14:36:12 +0430

    Add textbox, button and label on form 1 and form 6.

--- MyApplication/Form01.Designer.cs
+++ MyApplication/Form01.Designer.cs
@@ -29,19 +29,58 @@ namespace MyApplication
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.SuspendLayout();
-			// 
-			// Form01
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(484, 361);
-			this.Location = new System.Drawing.Point(0, 0);
-			this.Name = "Form01";
-			this.ResumeLayout(false);
+            this.test2Button = new System.Windows.Forms.Button();
+            this.test2Label = new System.Windows.Forms.Label();
+            this.test2TextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // test2Button
+            // 
+            this.test2Button.Location = new System.Drawing.Point(15, 34);
+            this.test2Button.Name = "test2Button";
+            this.test2Button.Size = new System.Drawing.Size(161, 35);
+            this.test2Button.TabIndex = 0;
+            this.test2Button.Text = "zeinab samani";
+            this.test2Button.UseVisualStyleBackColor = true;
+            // 
+            // test2Label
+            // 
+            this.test2Label.AutoSize = true;
+            this.test2Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.test2Label.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.test2Label.Location = new System.Drawing.Point(12, 9);
+            this.test2Label.Name = "test2Label";
+            this.test2Label.Size = new System.Drawing.Size(112, 17);
+            this.test2Label.TabIndex = 1;
+            this.test2Label.Text = "zeinab samani";
+            // 
+            // test2TextBox
+            // 
+            this.test2TextBox.Location = new System.Drawing.Point(130, 6);
+            this.test2TextBox.Name = "test2TextBox";
+            this.test2TextBox.Size = new System.Drawing.Size(118, 22);
+            this.test2TextBox.TabIndex = 2;
+            // 
+            // Form01
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(645, 444);
+            this.Controls.Add(this.test2TextBox);
+            this.Controls.Add(this.test2Label);
+            this.Controls.Add(this.test2Button);
+            this.Location = new System.Drawing.Point(0, 0);
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "Form01";
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 
-		#endregion
-	}
+        #endregion
+
+        private System.Windows.Forms.Button test2Button;
+        private System.Windows.Forms.Label test2Label;
+        private System.Windows.Forms.TextBox test2TextBox;
+    }
 }
--- MyApplication/Form06.Designer.cs
+++ MyApplication/Form06.Designer.cs
@@ -29,19 +29,58 @@ namespace MyApplication
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.SuspendLayout();
-			// 
-			// Form06
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(484, 361);
-			this.Location = new System.Drawing.Point(0, 0);
-			this.Name = "Form06";
-			this.ResumeLayout(false);
+            this.test2TextBox = new System.Windows.Forms.TextBox();
+            this.test2Label = new System.Windows.Forms.Label();
+            this.test2Button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // test2TextBox
+            // 
+            this.test2TextBox.Location = new System.Drawing.Point(130, 9);
+            this.test2TextBox.Name = "test2TextBox";
+            this.test2TextBox.Size = new System.Drawing.Size(118, 22);
+            this.test2TextBox.TabIndex = 5;
+            // 
+            // test2Label
+            // 
+            this.test2Label.AutoSize = true;
+            this.test2Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.test2Label.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.test2Label.Location = new System.Drawing.Point(12, 9);
+            this.test2Label.Name = "test2Label";
+            this.test2Label.Size = new System.Drawing.Size(112, 17);
+            this.test2Label.TabIndex = 4;
+            this.test2Label.Text = "zeinab samani";
+            // 
+            // test2Button
+            // 
+            this.test2Button.Location = new System.Drawing.Point(15, 53);
+            this.test2Button.Name = "test2Button";
+            this.test2Button.Size = new System.Drawing.Size(109, 36);
+            this.test2Button.TabIndex = 3;
+            this.test2Button.Text = "zeinab samani";
+            this.test2Button.UseVisualStyleBackColor = true;
+            // 
+            // Form06
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(645, 444);
+            this.Controls.Add(this.test2TextBox);
+            this.Controls.Add(this.test2Label);
+            this.Controls.Add(this.test2Button);
+            this.Location = new System.Drawing.Point(0, 0);
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "Form06";
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 
-		#endregion
-	}
+        #endregion
+
+        private System.Windows.Forms.TextBox test2TextBox;
+        private System.Windows.Forms.Label test2Label;
+        private System.Windows.Forms.Button test2Button;
+    }
 }
